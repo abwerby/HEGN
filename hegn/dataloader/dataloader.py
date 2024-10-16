961e300fbab3b2367a22fd976d1247d9dee262d3
Abdelrhman Werby <abdelrhmanwerby@gmail.com>
2024-10-16 02:35:23 +0200

    add scan object dataloader

--- hegn/dataloader/dataloader.py
+++ hegn/dataloader/dataloader.py
@@ -3,7 +3,8 @@
 import logging
 import os
 from typing import List
-
+import pickle
+import plyfile
 import h5py
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
@@ -122,54 +123,316 @@ class ModelNetHdf(Dataset):
     def to_category(self, i):
         return self._idx2category[i]
 
+class ScanObjetDataLoader:
+    def __init__(self, data_path=''):
+        self.DATA_PATH = data_path
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        sys.path.append(base_dir)
+        sys.path.append(os.path.join(base_dir, 'utils'))
 
+    def save_ply(self, points, filename, colors=None, normals=None):
+        vertex = np.core.records.fromarrays(points.transpose(), names='x, y, z', formats='f4, f4, f4')
+        n = len(vertex)
+        desc = vertex.dtype.descr
+
+        if normals is not None:
+            vertex_normal = np.core.records.fromarrays(normals.transpose(), names='nx, ny, nz', formats='f4, f4, f4')
+            assert len(vertex_normal) == n
+            desc = desc + vertex_normal.dtype.descr
+
+        if colors is not None:
+            vertex_color = np.core.records.fromarrays(colors.transpose() * 255, names='red, green, blue',
+                                                      formats='u1, u1, u1')
+            assert len(vertex_color) == n
+            desc = desc + vertex_color.dtype.descr
+
+        vertex_all = np.empty(n, dtype=desc)
+
+        for prop in vertex.dtype.names:
+            vertex_all[prop] = vertex[prop]
+
+        if normals is not None:
+            for prop in vertex_normal.dtype.names:
+                vertex_all[prop] = vertex_normal[prop]
+
+        if colors is not None:
+            for prop in vertex_color.dtype.names:
+                vertex_all[prop] = vertex_color[prop]
+
+        ply = plyfile.PlyData([plyfile.PlyElement.describe(vertex_all, 'vertex')], text=False)
+        ply.write(filename)
+
+    def load_pc_file(self, filename, suncg=False, with_bg=True):
+        pc = np.fromfile(os.path.join(self.DATA_PATH, filename), dtype=np.float32)
+
+        if suncg:
+            pc = pc[1:].reshape((-1, 3))
+        else:
+            pc = pc[1:].reshape((-1, 11))
+
+        if with_bg:
+            pc = np.array(pc[:, 0:3])
+            return pc
+        else:
+            filtered_idx = np.intersect1d(np.intersect1d(np.where(pc[:, -1] != 0)[0], np.where(pc[:, -1] != 1)[0]),
+                                          np.where(pc[:, -1] != 2)[0])
+            (values, counts) = np.unique(pc[filtered_idx, -1], return_counts=True)
+            max_ind = np.argmax(counts)
+            idx = np.where(pc[:, -1] == values[max_ind])[0]
+            pc = np.array(pc[idx, 0:3])
+            return pc
+
+    def load_data(self, filename, num_points=1024, suncg_pl=False, with_bg_pl=True):
+        with open(filename, 'rb') as handle:
+            data = pickle.load(handle)
+            print("Data loaded.")
+
+        pcs = []
+        labels = []
+
+        print(f"With BG: {with_bg_pl}")
+        for entry in data:
+            filename = entry["filename"].replace('objects_bin/', '')
+            pc = self.load_pc_file(filename, suncg=suncg_pl, with_bg=with_bg_pl)
+            label = entry['label']
+
+            if pc.shape[0] < num_points:
+                continue
+
+            pcs.append(pc)
+            labels.append(label)
+
+        print(f"Number of point clouds: {len(pcs)}")
+        print(f"Number of labels: {len(labels)}")
+
+        return pcs, labels
+
+    @staticmethod
+    def shuffle_points(pcs):
+        for pc in pcs:
+            np.random.shuffle(pc)
+        return pcs
+
+    @staticmethod
+    def get_current_data(pcs, labels, num_points):
+        sampled = []
+        for pc in pcs:
+            if pc.shape[0] < num_points:
+                print("Points too few.")
+                return None, None
+            else:
+                idx = np.arange(pc.shape[0])
+                np.random.shuffle(idx)
+                sampled.append(pc[idx[:num_points], :])
+
+        sampled = np.array(sampled)
+        labels = np.array(labels)
+
+        idx = np.arange(len(labels))
+        np.random.shuffle(idx)
+
+        return sampled[idx], labels[idx]
+
+    @staticmethod
+    def normalize_data(pcs):
+        for pc in pcs:
+            d = max(np.sum(np.abs(pc)**2, axis=-1)**(1./2))
+            pc /= d
+        return pcs
+
+    @staticmethod
+    def normalize_data_multiview(pcs, num_view=5):
+        pcs_norm = []
+        for pc_set in pcs:
+            pc = []
+            for j in range(num_view):
+                pc_view = pc_set[j, :, :]
+                d = max(np.sum(np.abs(pc_view)**2, axis=-1)**(1./2))
+                pc.append(pc_view/d)
+            pc = np.array(pc)
+            pcs_norm.append(pc)
+        pcs_norm = np.array(pcs_norm)
+        print("Normalized")
+        print(pcs_norm.shape)
+        return pcs_norm
+
+    @staticmethod
+    def center_data(pcs):
+        for pc in pcs:
+            centroid = np.mean(pc, axis=0)
+            pc -= centroid
+        return pcs
+
+    @staticmethod
+    def get_current_data_h5(pcs, labels, num_points):
+        idx_pts = np.arange(pcs.shape[1])
+        np.random.shuffle(idx_pts)
+
+        sampled = pcs[:, idx_pts[:num_points], :]
+
+        idx = np.arange(len(labels))
+        np.random.shuffle(idx)
+
+        return sampled[idx], labels[idx]
+
+    @staticmethod
+    def get_current_data_withmask_h5(pcs, labels, masks, num_points, shuffle=True):
+        idx_pts = np.arange(pcs.shape[1])
+
+        if shuffle:
+            np.random.shuffle(idx_pts)
+
+        sampled = pcs[:, idx_pts[:num_points], :]
+        sampled_mask = masks[:, idx_pts[:num_points]]
+
+        idx = np.arange(len(labels))
+
+        if shuffle:
+            np.random.shuffle(idx)
+
+        return sampled[idx], labels[idx], sampled_mask[idx]
+
+    @staticmethod
+    def get_current_data_parts_h5(pcs, labels, parts, num_points):
+        idx_pts = np.arange(pcs.shape[1])
+        np.random.shuffle(idx_pts)
+
+        sampled = pcs[:, idx_pts[:num_points], :]
+        sampled_parts = parts[:, idx_pts[:num_points]]
+
+        idx = np.arange(len(labels))
+        np.random.shuffle(idx)
+
+        return sampled[idx], labels[idx], sampled_parts[idx]
+
+    @staticmethod
+    def get_current_data_discriminator_h5(pcs, labels, types, num_points):
+        idx_pts = np.arange(pcs.shape[1])
+        np.random.shuffle(idx_pts)
+
+        sampled = pcs[:, idx_pts[:num_points], :]
+
+        idx = np.arange(len(labels))
+        np.random.shuffle(idx)
+
+        return sampled[idx], labels[idx], types[idx]
+
+    @staticmethod
+    def load_h5(h5_filename):
+        with h5py.File(h5_filename, 'r') as f:
+            data = f['data'][:]
+            label = f['label'][:]
+        return data, label
+
+    @staticmethod
+    def load_withmask_h5(h5_filename):
+        with h5py.File(h5_filename, 'r') as f:
+            data = f['data'][:]
+            label = f['label'][:]
+            mask = f['mask'][:]
+        return data, label, mask
+
+    @staticmethod
+    def load_discriminator_h5(h5_filename):
+        with h5py.File(h5_filename, 'r') as f:
+            data = f['data'][:]
+            label = f['label'][:]
+            model_type = f['type'][:]
+        return data, label, model_type
+
+    @staticmethod
+    def load_parts_h5(h5_filename):
+        with h5py.File(h5_filename, 'r') as f:
+            data = f['data'][:]
+            label = f['label'][:]
+            parts = f['parts'][:]
+        return data, label, parts
+
+    @staticmethod
+    def convert_to_binary_mask(masks):
+        binary_masks = []
+        for mask in masks:
+            binary_mask = np.ones(mask.shape)
+            bg_idx = np.where(mask == -1)
+            binary_mask[bg_idx] = 0
+            binary_masks.append(binary_mask)
+        return np.array(binary_masks)
+
+    @staticmethod
+    def flip_types(types):
+        return types == 0
 
 
 # to test the dataloader
 def main():
 
-    dataset = ModelNetHdf(dataset_path='data/modelnet40_ply_hdf5_2048', subset='train', categories=['airplane'])
-    print('Classes:', dataset.classes)
-    print('Number of samples:', len(dataset))
+    ## Test the ModelNetHdf class and the dataloader ##
+    # dataset = ModelNetHdf(dataset_path='data/modelnet40_ply_hdf5_2048', subset='train', categories=['airplane'])
+    # print('Classes:', dataset.classes)
+    # print('Number of samples:', len(dataset))
 
-    sample = dataset[0]
-    print('Sample:', sample)
-    print('Sample points:', sample['points'].shape)
-    print('Sample label:', sample['label'])
+    # sample = dataset[0]
+    # print('Sample:', sample)
+    # print('Sample points:', sample['points'].shape)
+    # print('Sample label:', sample['label'])
 
+    # # show the point cloud of the sample with open3d
+    # pcd1 = o3d.geometry.PointCloud()
+    # pcd1.points = o3d.utility.Vector3dVector(sample['points'][:, :3])
+
+
+    # transform = Compose([
+    #     Resampler(1024),
+    #     RandomJitter(scale=0.01, clip=0.05),
+    #     RandomTransformSE3(rot_mag=0, trans_mag=0.5, scale_range=(2.5, 5.5)),
+    # ])
+
+    # dataset = ModelNetHdf(dataset_path='data/modelnet40_ply_hdf5_2048', subset='train', categories=['airplane'],
+    #                       transform=transform)
+    # sample = dataset[0]
+    # print('Sample:', sample)
+    # print('Sample points:', sample['points'].shape)
+    # print('Sample label:', sample['label'])
+    # print('Sample idx:', sample['idx'])
+
+    # pcd = o3d.geometry.PointCloud()
+    # pcd1 = o3d.geometry.PointCloud()
+    # pcd1.points = o3d.utility.Vector3dVector(sample['points'][:, :3])
+    # pcd.points = o3d.utility.Vector3dVector(sample['points_ts'][:, :3])
+
+    # o3d.visualization.draw_geometries([pcd, pcd1])
+    # # print the bounding box of the point cloud
+
+
+    # dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=0)
+    # for i_batch, sample_batched in enumerate(dataloader):
+    #     print(i_batch, sample_batched['points'].size(), sample_batched['label'])
+    #     if i_batch == 1:
+    #         break
+    
+    ## Test the PointCloudDataLoader class ##
+    loader = ScanObjetDataLoader()
+    
+    h5_file = '/export/home/werbya/thesis/HEGN/data/scanobjectnn/main_split/test_objectdataset_augmented25_norot.h5'
+    h5_data, h5_labels = loader.load_h5(h5_file)
+
+    print(f"Loaded H5 data shape: {h5_data.shape}")
+    print(f"Loaded H5 labels shape: {h5_labels.shape}")
+
+    # Get a batch of data from H5 file
+    num_points = 1024
+    h5_batch_pcs, h5_batch_labels = loader.get_current_data_h5(h5_data, h5_labels, num_points)
+
+    print(f"H5 batch shape: {h5_batch_pcs.shape}")
+    print(f"H5 labels shape: {h5_batch_labels.shape}")
+
+    binary_masks = loader.convert_to_binary_mask(np.random.randint(-1, 2, size=(10, 1024)))
+    print(f"Binary masks shape: {binary_masks.shape}")
+    
     # show the point cloud of the sample with open3d
     pcd1 = o3d.geometry.PointCloud()
-    pcd1.points = o3d.utility.Vector3dVector(sample['points'][:, :3])
-
-
-    transform = Compose([
-        Resampler(1024),
-        RandomJitter(scale=0.01, clip=0.05),
-        RandomTransformSE3(rot_mag=0, trans_mag=0.5, scale_range=(2.5, 5.5)),
-    ])
-
-    dataset = ModelNetHdf(dataset_path='data/modelnet40_ply_hdf5_2048', subset='train', categories=['airplane'],
-                          transform=transform)
-    sample = dataset[0]
-    print('Sample:', sample)
-    print('Sample points:', sample['points'].shape)
-    print('Sample label:', sample['label'])
-    print('Sample idx:', sample['idx'])
-
-    pcd = o3d.geometry.PointCloud()
-    pcd1 = o3d.geometry.PointCloud()
-    pcd1.points = o3d.utility.Vector3dVector(sample['points'][:, :3])
-    pcd.points = o3d.utility.Vector3dVector(sample['points_ts'][:, :3])
-
-    o3d.visualization.draw_geometries([pcd, pcd1])
-    # print the bounding box of the point cloud
-
-
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=0)
-    for i_batch, sample_batched in enumerate(dataloader):
-        print(i_batch, sample_batched['points'].size(), sample_batched['label'])
-        if i_batch == 1:
-            break
+    pcd1.points = o3d.utility.Vector3dVector(h5_batch_pcs[0, :, :])
+    o3d.visualization.draw_geometries([pcd1])
 
 
 if __name__ == '__main__':
--- hegn/registration/pointcloudregistration.py
+++ hegn/registration/pointcloudregistration.py
@@ -25,14 +25,28 @@ class PointCloudRegistration:
         self.model = HEGN(args=Args()).to(self.device)
         self.model.load_state_dict(torch.load(checkpoint_path, map_location=self.device))
         self.model.eval()
+    
+    def _resample(self, points, k):
+        """Resamples the points such that there is exactly k points.
 
-        # Initialize transforms
-        self.transform = Compose([
-            Resampler(1024, resample_both=True),
-            RandomJitter(scale=0.01, clip=0.05)
-        ])
+        If the input point cloud has <= k points, it is guaranteed the
+        resampled point cloud contains every point in the input.
+        If the input point cloud has > k points, it is guaranteed the
+        resampled point cloud does not contain repeated point.
+        """
 
-    def preprocess(self, points: np.ndarray) -> torch.Tensor:
+        if k <= points.shape[0]:
+            rand_idxs = np.random.choice(points.shape[0], k, replace=False)
+            return points[rand_idxs, :]
+        elif points.shape[0] == k:
+            return points
+        else:
+            rand_idxs = np.concatenate([np.random.choice(points.shape[0], points.shape[0], replace=False),
+                                        np.random.choice(points.shape[0], k - points.shape[0], replace=True)])
+            return points[rand_idxs, :]
+
+
+    def preprocess(self, points: np.ndarray, num_points: int = 10000) -> torch.Tensor:
         """Preprocess the input point cloud."""
         if not isinstance(points, np.ndarray):
             raise TypeError("Input must be a numpy array")
@@ -43,8 +57,8 @@ class PointCloudRegistration:
         # Convert to torch tensor
         points = torch.from_numpy(points).float().to(self.device)
         
-        # Apply transforms
-        # points = self.transform({'points': points})['points']
+        # sample uniformly to num_points
+        points = self._resample(points, num_points)
         
         # Transpose to (3, N) as expected by the model
         return points.transpose(1, 0).unsqueeze(0)
@@ -96,10 +110,11 @@ class PointCloudRegistration:
             target (np.ndarray): Target point cloud
             aligned_source (np.ndarray, optional): Aligned source point cloud
         """
-        def create_point_cloud(points, color):
+        def create_point_cloud(points, color, voxel_size=0.05):
             pcd = o3d.geometry.PointCloud()
             pcd.points = o3d.utility.Vector3dVector(points)
             pcd.paint_uniform_color(color)
+            pcd = pcd.voxel_down_sample(voxel_size=voxel_size)
             return pcd
 
         source_pcd = create_point_cloud(source, [1, 0, 0])  # Red
@@ -107,9 +122,11 @@ class PointCloudRegistration:
 
         if aligned_source is not None:
             aligned_pcd = create_point_cloud(aligned_source, [0, 0, 1])  # Blue
-            o3d.visualization.draw_geometries([source_pcd, target_pcd, aligned_pcd])
+            # o3d.visualization.draw_geometries([source_pcd, target_pcd, aligned_pcd])
+            o3d.io.write_point_cloud("aligned.ply", source_pcd + target_pcd + aligned_pcd)
         else:
-            o3d.visualization.draw_geometries([source_pcd, target_pcd])
+            # o3d.visualization.draw_geometries([source_pcd, target_pcd])
+            o3d.io.write_point_cloud("aligned.ply", source_pcd + target_pcd)
 
 # Example usage
 if __name__ == "__main__":
@@ -117,8 +134,25 @@ if __name__ == "__main__":
     pcr = PointCloudRegistration(checkpoint_path="/export/home/werbya/thesis/HEGN/checkpoints/hegn.pth")
 
     # Load your point clouds (replace with your actual data)
-    source = np.random.rand(1000, 3)
-    target = np.random.rand(1000, 3)
+    source = o3d.io.read_point_cloud("/export/home/werbya/thesis/before.ply")
+    target = o3d.io.read_point_cloud("/export/home/werbya/thesis/after.ply")
+    
+    # downsample the point clouds
+    # source = source.voxel_down_sample(voxel_size=0.05)
+    # target = target.voxel_down_sample(voxel_size=0.05)
+    
+    source = np.asarray(source.points)
+    target = np.asarray(target.points)
+    
+    # apply random translation to the source point cloud
+    t_rand = np.random.uniform(-0.1, 0.1, size=(3,))
+    source += t_rand
+    
+    
+    
+    
+    print("Source shape:", source.shape)
+    print("Target shape:", target.shape)
 
     # Perform registration
     aligned_source, R, S, t = pcr.register(source, target)
